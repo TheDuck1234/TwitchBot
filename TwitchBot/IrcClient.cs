1179687f19b50f7396aaf7b4bec707e13f3cf892
Anders Petersen <Jinx.petersen@gmail.com>
2015-10-21 04:23:14 +0200

    The Rest of the program

---
+++ TwitchBot/IrcClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TwitchBot
+{
+    class IrcClient
+    {
+        private readonly string _userName;
+        private string _channel;
+
+        private TcpClient _tcpClient;
+        private StreamReader _inputReader;
+        private StreamWriter _outputWriter;
+
+        public IrcClient(string ip, int port,string username,string password)
+        {
+            _userName = username;
+            _tcpClient = new TcpClient(ip,port);
+            _inputReader = new StreamReader(_tcpClient.GetStream());
+            _outputWriter = new StreamWriter(_tcpClient.GetStream());
+
+            _outputWriter.WriteLine("PASS " + password);
+            _outputWriter.WriteLine("NICK " + username );
+            _outputWriter.WriteLine("USER "+ username+ " 8 * :"+ username);
+            _outputWriter.Flush();
+        }
+
+        public void JoinRoom(string channel)
+        {
+            _channel = channel;
+            _outputWriter.WriteLine("JOIN #"+ channel);
+            _outputWriter.Flush();
+        }
+
+        public void SendIrcMessage(string message)
+        {
+            _outputWriter.WriteLine(message);
+            _outputWriter.Flush();
+        }
+
+        public void SendChatMessage(string message)
+        {
+            SendIrcMessage(":"+_userName+"!"+_userName+"@"+_userName
+                +"tmi.twitch.tv PRIVMSG #"+ _channel+ " :"+message);
+        }
+
+        public string ReadMessage()
+        {
+            string message = _inputReader.ReadLine();
+            return message;
+        }
+    }
+}
---
+++ TwitchBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+    class Program
+    {
+
+        //password from twitchapps.com/tmi
+
+        static void Main(string[] args)
+        {
+            var irc = new IrcClient("irc.twitch.tv",6667, "cogwheelbot", "oauth:ffiiryof5w501xlz43qbk75k9xcmv1");
+            irc.JoinRoom("faddei");
+            var stopWatch = Stopwatch.StartNew(); 
+            while (true)
+            {
+                string message = irc.ReadMessage();
+
+                if(!string.IsNullOrEmpty(message)) {
+                    Console.WriteLine(message);
+                    if (message.ToLower().Contains("!hallo"))
+                    {
+                        irc.SendChatMessage("Testing Testing");
+                    }
+                    if (message.ToLower().Contains("!uptime"))
+                    {
+                        DateTime time = new DateTime(stopWatch.ElapsedTicks);
+                        string uptime = time.ToString("HH:mm:ss");
+                        irc.SendChatMessage("Time: "+uptime);
+
+                    }
+                }
+
+            }
+                
+        }
+    }
+}
